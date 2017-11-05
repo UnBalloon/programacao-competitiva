da2521d86f4c04f20b927b3c81ad2d49c7761fcb
GTaumaturgo <gtaumaturg@gmail.com>
2017-11-05 14:46:37 -0200

    exemplo primo rapido adicionado

---
+++ Aula1/PrimoRapido/PrimoRapido.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int eh_primo(long long n)
+{						// i^2 <= n é a a mesma coisa que i <= sqrt(n)	
+	for (long long i = 2; i*i <= n; ++i)
+	{
+		if(n % i == 0)
+			return 0; // nao é primo;
+	}
+	return 1; //chegou ate o final, entao é primo
+}
+int main(){
+
+	int n = 1e9 + 7;
+
+
+	if(eh_primo(n))
+		printf("%d é primo!\n", n);
+	else
+		printf("%d não é primo!\n",n);
+
+
+}
