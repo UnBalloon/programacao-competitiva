6451c584f686430a947a0f23329cb645281ba1cd
Gabriel Taumaturgo <gabrieloliveiraunb@gmail.com>
2017-11-08 11:45:04 -0200

    Update pair.cpp

--- Aula2/00_IO_E_TIPOS/pair.cpp
+++ Aula2/00_IO_E_TIPOS/pair.cpp
@@ -7,7 +7,7 @@ int main(){
 	//C++ tem o tipo pair
 	// que é um par ordenado matemático
 
-	pair<int,int> par1 = make_pair(0,1);
+	pair<int,int> par1 = make_pair(0,1); //make_pair constroi um par.
 	pair<int,int> par2 = make_pair(1,0);
 
 
@@ -20,8 +20,19 @@ int main(){
 	else if(par1 < par2)
 		printf("par2 é maior\n");
 	else
-		printf("sao iguais\n");
+		printf("par1 e par2 sao iguais\n");
 
 	
+	// nao precisa ser um par de inteiros
+	pair<int,string> par3 = make_pair(0,"oie");
+	pair<int,string> par4 = make_pair(0,"aie");
+	
+	// nesse exemplo, o elemento da esqueda é igual,  e o desempate será feito pelo da direita.
+	if(par3 > par4)
+		printf("par3 é maior\n");
+	else if(par4 > par3)
+		printf("par4 é maior\n");
+	else
+		printf("par3 e par4 sao iguais);
 }
 	
