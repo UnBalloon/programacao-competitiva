3d7f8c9a2a8b5fb31a00f49fe6d56d4ad655de8c
Gabriel Taumaturgo <gabrieloliveiraunb@gmail.com>
2017-11-08 11:35:22 -0200

    Update strings.cpp

--- Aula2/00_IO_E_TIPOS/strings.cpp
+++ Aula2/00_IO_E_TIPOS/strings.cpp
@@ -10,18 +10,23 @@ int main(){
 	cin >> s1;
 	// c++ permite concatenação de
 	// strings usando o operador +
-	printf("s + s1:\n");
+	printf("s + s1:\n"); // nada impede de continuar usando printf
 	cout << s + s1 << '\n';
 	printf("s1 + s:\n");
 	cout << s1 + s << '\n';
 
 	// c++ também permite comparação de strings
-	// com base na ordem lexicográfica (dicionário)
 	if(s > s1)
 		printf("s1 vem antes na ordem lexicográfica\n");
 	else if (s < s1)
 		printf("s vem antes na ordem lexicográfica\n");
 	else
 		printf("s e s1 sao iguais\n");
+	
+	string numero = "12345";	
+	int n = stoi(numero); //stoi significa string to int (se quiser long long é stoll)
+	printf("%d\n",n);
+	numero = to_string(n);
+	cout << numero << '\n'; // para fazer IO com strings, precisa ser com cin e cout.
 }
 	
