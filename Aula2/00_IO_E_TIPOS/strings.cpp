b7f74dac16b86b13d5e767fe46069973ed438491
Gabriel Taumaturgo <gabrieloliveiraunb@gmail.com>
2017-11-08 12:19:03 -0200

    Update strings.cpp

--- Aula2/00_IO_E_TIPOS/strings.cpp
+++ Aula2/00_IO_E_TIPOS/strings.cpp
@@ -5,24 +5,27 @@ using namespace std;
 int main(){
 	
 	// C++ tem o tipo string
-	string s,s1;
-	cin >> s;
+	string s1,s2;
+	printf("Digite a primeira string(s1)\n");
 	cin >> s1;
+	printf("Digite a segunda string(s2)\n");
+	cin >> s2;
 	// c++ permite concatenação de
 	// strings usando o operador +
-	printf("s + s1:\n"); // nada impede de continuar usando printf
-	cout << s + s1 << '\n';
-	printf("s1 + s:\n");
-	cout << s1 + s << '\n';
+	printf("s1 + s2:\n"); // nada impede de continuar usando printf
+	cout << s1 + s2 << '\n';
+	printf("s2 + s1:\n");
+	cout << s2 + s1 << '\n';
 
 	// c++ também permite comparação de strings
-	if(s > s1)
-		printf("s1 vem antes na ordem lexicográfica\n");
-	else if (s < s1)
-		printf("s vem antes na ordem lexicográfica\n");
+	if(s1 > s2)
+		printf("s2 vem antes na ordem ASCII\n");
+	else if (s1 < s2)
+		printf("s1 vem antes na ordem ASCII\n");
 	else
-		printf("s e s1 sao iguais\n");
+		printf("s1 e s2 sao iguais\n");
 	
+	printf("\n\n\n");	
 	string numero = "12345";	
 	int n = stoi(numero); //stoi significa string to int (se quiser long long é stoll)
 	printf("%d\n",n);
